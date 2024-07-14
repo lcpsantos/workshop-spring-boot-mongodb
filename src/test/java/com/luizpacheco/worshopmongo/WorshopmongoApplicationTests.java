749512c52167370849ac305384534470bf07fbee
Luiz César Pacheco <lcpsantos@gmail.com>
2024-07-13 23:06:33 -0300

    Git init

---
+++ src/test/java/com/luizpacheco/worshopmongo/WorshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizpacheco.worshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
