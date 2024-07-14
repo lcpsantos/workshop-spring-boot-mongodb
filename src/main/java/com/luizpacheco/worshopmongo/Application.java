0bf6d475d23f930507297c2b75de73abb6be2695
Luiz César Pacheco <lcpsantos@gmail.com>
2024-07-14 00:14:41 -0300

    REST funcionando

--- src/main/java/com/luizpacheco/worshopmongo/Application.java
+++ src/main/java/com/luizpacheco/worshopmongo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WorshopmongoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WorshopmongoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
