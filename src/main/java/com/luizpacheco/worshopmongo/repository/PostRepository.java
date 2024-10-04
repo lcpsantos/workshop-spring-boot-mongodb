140cbbab0ce7b2e941af3cf261df628bf0791bcd
Luiz César Pacheco <lcpsantos@gmail.com>
2024-10-03 22:58:56 -0300

    Consulta simples com query methods

--- src/main/java/com/luizpacheco/worshopmongo/repository/PostRepository.java
+++ src/main/java/com/luizpacheco/worshopmongo/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.luizpacheco.worshopmongo.repository;
 
 import com.luizpacheco.worshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,6 +11,10 @@ import java.util.List;
 @Repository
 @Transactional
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    //Consulta simples com @Query
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }") // ?0 = first parameter of the method (text) and 'i' = case insensitive
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
